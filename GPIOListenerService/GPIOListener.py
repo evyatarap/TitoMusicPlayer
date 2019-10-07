1cfb1133456de8d566a87e595d9f0f4148f0704f
Evyatar Apteker <evyatarap@gmail.com>
2019-10-07 23:18:50 +0300

    Adding print

--- GPIOListenerService/GPIOListener.py
+++ GPIOListenerService/GPIOListener.py
@@ -5,6 +5,7 @@ from time import sleep
 
 
 if __name__ == '__main__':
+    print("GPIOListener Service started...")
     btn_reset_wifi = Button(5)
     while True:
         if btn_reset_wifi.is_pressed:
