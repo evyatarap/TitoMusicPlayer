9e51f5d3d4827ac274b1521f3c7d8847f671286c
Evyatar Apteker <evyatarap@gmail.com>
2019-10-12 17:30:52 +0300

    Fixing the select pin

--- GPIOListenerService/GPIOListener.py
+++ GPIOListenerService/GPIOListener.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     print("GPIOListener Service started...")
     btn_reset_wifi = Button(PIN_WIFI_RESET)
-    btn_spk_en = Button(PIN_WIFI_RESET)
+    btn_spk_en = Button(PIN_SPEAKER_EN)
     while True:
         if btn_reset_wifi.is_pressed:
             print("Wifi reset pressed")
