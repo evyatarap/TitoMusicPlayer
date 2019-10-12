ccef542a2f2d2f7dea9b902edf173d93a5e30ccf
Evyatar Apteker <evyatarap@gmail.com>
2019-10-12 17:29:36 +0300

    Adding speaker enabled button

--- GPIOListenerService/GPIOListener.py
+++ GPIOListenerService/GPIOListener.py
@@ -2,10 +2,18 @@ from gpiozero import Button
 from time import sleep
 
 
+
 if __name__ == '__main__':
+
+    PIN_WIFI_RESET=5
+    PIN_SPEAKER_EN=4
+
     print("GPIOListener Service started...")
-    btn_reset_wifi = Button(5)
+    btn_reset_wifi = Button(PIN_WIFI_RESET)
+    btn_spk_en = Button(PIN_WIFI_RESET)
     while True:
         if btn_reset_wifi.is_pressed:
             print("Wifi reset pressed")
+        if btn_spk_en.is_pressed:
+            print("Speaker is enabled")
         sleep(1)
