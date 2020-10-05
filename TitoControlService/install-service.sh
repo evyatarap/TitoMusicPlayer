686f8ebe778546bb2c80fb835f18be24c9a0ebcf
Evyatar Apteker <evyatarap@gmail.com>
2020-10-05 21:29:22 +0300

    Adding pip install to the service setup

--- TitoControlService/install-service.sh
+++ TitoControlService/install-service.sh
@@ -4,6 +4,9 @@ SRC_INSTALLATION_DIR=`dirname $0`
 DEST_INSTALLATION_DIR="/usr/local/bin/TitoControlService"
 APP_LOG_DIR="/var/log/TitoControlService"
 
+# install pip requirements
+sudo pip3 install -r requirements.txt
+
 # create service directories
 sudo mkdir $DEST_INSTALLATION_DIR
 sudo mkdir $APP_LOG_DIR
